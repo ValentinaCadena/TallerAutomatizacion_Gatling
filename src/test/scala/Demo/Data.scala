f8783143d89c9a00b933e53a7f737dd61a44205e
Valentina Cadena Zapata <82690199+ValentinaCadena@users.noreply.github.com>
2025-06-25 23:38:27 -0500

    Update Data.scala

--- src/test/scala/Demo/Data.scala
+++ src/test/scala/Demo/Data.scala
@@ -4,4 +4,5 @@ object Data{
     val url= "https://thinking-tester-contact-list.herokuapp.com/"
     val email = "valentina.prueba@gmail.com"
     val password = "Hola12345"
+    val authToken = "685b61fb18142400152afa2a"
 }
