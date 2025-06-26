004dd913553fb079fa57c0e08b96e9a2d15c3c69
Valentina Cadena Zapata <82690199+ValentinaCadena@users.noreply.github.com>
2025-06-26 12:04:58 -0500

    Create InvalidLogin.scala

---
+++ src/test/scala/Demo/InvalidLogin.scala
@@ -0,0 +1,29 @@
+package Demo
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import Demo.Data._
+
+class InvalidLoginTest extends Simulation{
+
+  // 1 Http Conf
+  val httpConf = http.baseUrl(url)
+    .acceptHeader("application/json")
+    //Verificar de forma general para todas las solicitudes
+    .check(status.is(200))
+
+  // 2 Scenario Definition
+  val scn = scenario("Invalid Login").
+    exec(http("login")
+      .get(s"users/login")
+      .body(StringBody(s"""{"email": "$wrongemail@email.com", "password": "$wrongpassword"}""")).asJson
+       //Validar status 200 del servicio
+      .check(status.is(401))
+      .check(bodyString.is("Incorrect username or password"))
+    )
+
+  // 3 Load Scenario
+  setUp(
+    scn.inject(rampUsersPerSec(5).to(15).during(30))
+  ).protocols(httpConf);
+}
